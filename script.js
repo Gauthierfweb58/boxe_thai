0ebd2ba576428f2a9785917dea17ce03a4935a72
AlexisMetton <alexis.metton@gmail.com>
2022-08-02 16:56:35 +0200

    nav responsive

--- script.js
+++
@@ -1,7 +0,0 @@
-function bulleDisparition(){
-    if(window.innerWidth<=920 && document.getElementById("Checkbox").checked == true){
-}else {
-    document.getElementById('menu').style.rotate = "90";
-    document.getElementById('menu').style.transition = "250ms";
-}
-}
